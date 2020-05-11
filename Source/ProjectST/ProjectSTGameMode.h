0a7710f4b0bc5ce119c416e48a35397b37e65a98
Nicky Jones <nicky_woz_ere@hotmail.com>
2020-05-11 15:17:37 +0100

    ProjectST's starter project, implemented Create Session and Find/Join Sessions

---
+++ Source/ProjectST/ProjectSTGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectSTGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectSTGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectSTGameMode();
+};
+
+
+
